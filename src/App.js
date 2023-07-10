f66d09d3556a7447300ee6641120f704b0b98e93
lenayork <lenayork@tut.by>
2023-07-10 22:38:51 +0300

    feat: add basic structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="header">
+        
+      </header>
+      <section className="queries"></section>
+      <section className="cards"></section>
+    </div>
+  );
+}
+
+export default App;
