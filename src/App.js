ca56f91adc9b64c879e585cd62a79387985b8b70
lenayork <lenayork@tut.by>
2023-07-10 22:52:11 +0300

    feat: add header and footer

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ function App() {
   return (
     <div className="container">
       <header className="header">
-        
+        <h1>harry potter</h1>
+        <h2>characters</h2>
       </header>
       <section className="queries"></section>
       <section className="cards"></section>
+      <footer className="footer">
+        <p>All rights reserved &copy; 2023</p>
+      </footer>
     </div>
   );
 }
