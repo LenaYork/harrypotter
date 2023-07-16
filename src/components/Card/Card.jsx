3af0570cb66daacf2631a38af50d8437d1aeae78
lenayork <lenayork@tut.by>
2023-07-16 16:59:33 +0300

    fix: correct display of unknown house name

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,13 @@
 import "./Card.css";
 
 export const Card = ({img, name, species, house, dateOfBirth, wand, ancestry, patronus, actor}) => {
+    const currentCharacterHouse = house === "house" ? "unknown" : house;
     return(
         <div className="card">
             <img src={img} alt="harry potter pic"/>
             <h3>{name}</h3>
             <p className="card-subtitle">Species: <span className="important">{species || "unknown"}</span></p>
-            <p className="card-subtitle">House: <span className="important">{house || "unknown"}</span></p>
+            <p className="card-subtitle">House: <span className="important">{currentCharacterHouse}</span></p>
             <p className="card-subtitle">Date of birth: <span className="important">{dateOfBirth || "unknown"}</span></p>
             <p className="card-subtitle">Wand: <span className="important">{wand || "unknown"}</span></p>
             <p className="card-subtitle">Ancestry: <span className="important">{ancestry || "unknown"}</span></p>
